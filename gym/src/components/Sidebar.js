82d574d267eacb4a3b1c64cb956aa716aed2b69b
Meshoeternity <Mashael.othman.abt.js@tuwaiq.edu.sa>
2022-01-13 10:19:16 +0300

    done

--- gym/src/components/Sidebar.js
+++ gym/src/components/Sidebar.js
@@ -1,27 +1,20 @@
 import Box from "@mui/material/Box"
 import Drawer from "@mui/material/Drawer"
-import CssBaseline from "@mui/material/CssBaseline"
-import AppBar from "@mui/material/AppBar"
-import Toolbar from "@mui/material/Toolbar"
 import List from "@mui/material/List"
-import Typography from "@mui/material/Typography"
 import Divider from "@mui/material/Divider"
 import ListItem from "@mui/material/ListItem"
 import ListItemIcon from "@mui/material/ListItemIcon"
 import ListItemText from "@mui/material/ListItemText"
-import InboxIcon from "@mui/icons-material/MoveToInbox"
-import MailIcon from "@mui/icons-material/Mail"
 import MovieCreationIcon from "@mui/icons-material/MovieCreation"
 import AddReactionIcon from "@mui/icons-material/AddReaction"
 import RecentActorsIcon from "@mui/icons-material/RecentActors"
 import GroupIcon from "@mui/icons-material/Group"
-import TheatersIcon from "@mui/icons-material/Theaters"
 import { createTheme, ThemeProvider } from "@mui/material"
 import { Link } from "react-router-dom"
 import LoginIcon from "@mui/icons-material/Login"
-import GroupAddIcon from "@mui/icons-material/GroupAdd"
 import { useContext } from "react"
 import SportsContext from "../utils/SportsContext"
+import { CgGym } from "react-icons/cg";
 
 const drawerWidth = 240
 
@@ -42,7 +35,7 @@ export default function PermanentDrawerLeft() {
           
           primary: { main: "rgb(102, 157, 246)" },
         
-          background: { paper: "rgb(5, 30, 52)" },
+          background: { paper: "rgb(87, 5, 48)" },
         },
       })}
     >
@@ -62,8 +55,9 @@ export default function PermanentDrawerLeft() {
         <List>
           <ListItem>
             <ListItemIcon>
-              <TheatersIcon />
+            <CgGym />
             </ListItemIcon>
+           
             <ListItemText primary="My GYM dashboard" />
           </ListItem>
         </List>
